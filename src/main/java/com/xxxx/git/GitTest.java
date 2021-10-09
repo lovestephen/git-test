40dfccde5a04be1ccdb6ff9780a25b1865d8dc7d
Darren <2025578381@qq.com>
2021-10-09 17:55:45 +0800

    first commit

---
+++ src/main/java/com/xxxx/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.xxxx.git;
+
+/**
+ * @Author: DarrenLin
+ * @Date: 2021/10/9 17:45
+ * @Description: 可以一个添加到暂存区，也可以把整个项目添加到暂存区
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
