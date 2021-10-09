8d48d70b347bc542a72851619560f13ba41c8c69
Darren <2025578381@qq.com>
2021-10-09 18:17:34 +0800

    hot-fix commit

--- src/main/java/com/xxxx/git/GitTest.java
+++ src/main/java/com/xxxx/git/GitTest.java
@@ -8,5 +8,7 @@ package com.xxxx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+
+        System.out.println("hot-fix");
     }
 }
